b940ef7102c0de49e84b469026edb26b83372aa2
sakuni rajapaksha <sakunirajapaksha007@gmail.com>
2026-05-18 10:21:02 +0530

    Fixed production API URL

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+
 const API = axios.create({
-baseURL: "http://localhost:5000/api/jobs",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL,
 });
 
 // Attach JWT token automatically
